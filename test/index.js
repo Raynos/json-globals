ccb57f3a54f8b045f9198c274c3a54f6349cb19f
Aleksey Smolenchuk <aleksey@uber.com>
2016-02-11 11:33:21 -0800

    use serialize-javascript instead of JSON.stringify to sanitize strings

--- test/index.js
+++ test/index.js
@@ -8,16 +8,23 @@ test("JSONGlobals is a function", function (assert) {
 })
 
 test("Can globalify stuff", function (assert) {
-    var html = JSONGlobals({ user: { name: "bob" } })
+    var html = JSONGlobals({
+        plain: { name: "bob" },
+        regex: /foo/,
+        malicious: '</script>'
+    })
 
     assert.equal(html,
         "if (!window.__JSON_GLOBALS_) {\n" +
         "    window.__JSON_GLOBALS_ = {}\n" +
         "}\n" +
-        "window.__JSON_GLOBALS_[\"user\"] = {\n" +
-        "    \"name\": \"bob\"\n" +
-        "}\n"
+
+        "window.__JSON_GLOBALS_[\"plain\"] = {\"name\":\"bob\"}\n" +
+        "window.__JSON_GLOBALS_[\"regex\"] = /foo/\n" +
+        "window.__JSON_GLOBALS_[\"malicious\"] = " +
+            "\"\\u003C\\u002Fscript\\u003E\"\n"
     )
 
     assert.end()
 })
+
