75dc75acbfa12d82a41717078ff3786f57253a35
Aleksey Smolenchuk <lxe@lxe.co>
2016-06-22 21:01:12 -0700

    add moar semicolons

--- get.js
+++ get.js
@@ -1,12 +1,12 @@
-var document = require("global/document")
+var document = require("global/document");
 
-module.exports = getJSONGlobal
+module.exports = getJSONGlobal;
 
 var globals;
 function getJSONGlobal(key) {
     if (!globals) {
-      var jsonGlobalsElement = document.getElementById('json-globals')
-      globals = JSON.parse(jsonGlobalsElement.textContent)
+      var jsonGlobalsElement = document.getElementById('json-globals');
+      globals = JSON.parse(jsonGlobalsElement.textContent);
     }
     return globals[key];
 }
